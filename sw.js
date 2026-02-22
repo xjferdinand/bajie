00fb233eccc472fa14ab08a7d4d7c00c870bb7bd
xjferdinand <xjferdinand@gmail.com>
2026-02-22 19:01:09 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+// sw.js
+const CACHE_NAME = "bajie-super-t-v1"; // ← 每次更新网页就改这个版本号
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./icons/icon-180.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k === CACHE_NAME ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
